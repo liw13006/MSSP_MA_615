d016cf151fba1e3ae035f3074983466506e7691a
Weiling Li <nova@dhcp-wifi-8021x-155-41-58-43.bu.edu>
2019-09-04 16:28:57 -0400

    Sep 4th

--- Esquissertest.R
+++ Esquissertest.R
@@ -1,3 +1,10 @@
 library(esquisse)
 data(mtcars)
 esquisser(mtcars)
+library(ggplot2)
+
+ggplot(data = mtcars) +
+  aes(x = hp, y = mpg, size = cyl) +
+  geom_point(color = '#0c4c8a') +
+  theme_minimal()
+View(mtcars)
